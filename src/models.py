39833ccc79750fdd4d2d855e90b427a5bbf366a9
Peeranuth Kehasukcharoen <peeranuth.k@gmail.com>
2020-11-08 13:24:16 +0700

    Model bug & datetime

--- src/models.py
+++ src/models.py
@@ -1,35 +1,35 @@
 import neomodel
-import datetime
+from datetime import datetime
 from neomodel.properties import *
 
 class BaseEntity(neomodel.StructuredNode):
     name = neomodel.StringProperty(unique_index=True)
     create_at = DateTimeProperty(
-        default=datetime.now() # no specific timezone 
+        default=lambda: datetime.now() # no specific timezone 
     )
     count = neomodel.IntegerProperty(default=1)
 
     #relation from Task to ...
-    task_usedFor = neomodel.RelationshipTo('Task', 'Used-for',model=UsedFor)
-    task_partOf = neomodel.RelationshipTo('Task', 'Part-of',model=PartOf)
-    task_featureOf = neomodel.RelationshipTo('Task', 'Feature-of',model=FeatureOf)
-    task_compare = neomodel.RelationshipTo('Task', 'Compare',model=Compare)
-    task_hyponymOf = neomodel.RelationshipTo('Task', 'Hyponym-of',model=HyponymOf)
-    task_evaluateFor = neomodel.RelationshipTo('Task', 'Evaluate-for',model=EvaluateFor)
-    task_referTo = neomodel.RelationshipTo('Task', 'Refer-to',model=ReferTo)
-    task_isA = neomodel.RelationshipTo('Task', 'Is-a',model=IsA)
-    task_appearIn = neomodel.RelationshipTo('Task', 'Appear-in',model=AppearIn)
+    task_usedFor = neomodel.RelationshipTo('Task', 'Used-for')
+    task_partOf = neomodel.RelationshipTo('Task', 'Part-of')
+    task_featureOf = neomodel.RelationshipTo('Task', 'Feature-of')
+    task_compare = neomodel.RelationshipTo('Task', 'Compare')
+    task_hyponymOf = neomodel.RelationshipTo('Task', 'Hyponym-of')
+    task_evaluateFor = neomodel.RelationshipTo('Task', 'Evaluate-for')
+    task_referTo = neomodel.RelationshipTo('Task', 'Refer-to')
+    task_isA = neomodel.RelationshipTo('Task', 'Is-a')
+    task_appearIn = neomodel.RelationshipTo('Task', 'Appear-in')
 
     # relation from method to ...
-    method_usedFor = neomodel.RelationshipTo('Method', 'Used-for',model=UsedFor)
-    method_partOf = neomodel.RelationshipTo('Method', 'Part-of',model=PartOf)
-    method_featureOf = neomodel.RelationshipTo('Method', 'Feature-of',model=FeatureOf)
-    method_compare = neomodel.RelationshipTo('Method', 'Compare',model=Compare)
-    method_hyponymOf = neomodel.RelationshipTo('Method', 'Hyponym-of',model=HyponymOf)
-    method_evaluateFor = neomodel.RelationshipTo('Method', 'Evaluate-for',model=EvaluateFor)
-    method_referTo = neomodel.RelationshipTo('Method', 'Refer-to',model=ReferTo)
-    method_isA = neomodel.RelationshipTo('Method', 'Is-a',model=IsA)
-    method_appearIn = neomodel.RelationshipTo('Method', 'Appear-in',model=AppearIn)
+    method_usedFor = neomodel.RelationshipTo('Method', 'Used-for')
+    method_partOf = neomodel.RelationshipTo('Method', 'Part-of')
+    method_featureOf = neomodel.RelationshipTo('Method', 'Feature-of')
+    method_compare = neomodel.RelationshipTo('Method', 'Compare')
+    method_hyponymOf = neomodel.RelationshipTo('Method', 'Hyponym-of')
+    method_evaluateFor = neomodel.RelationshipTo('Method', 'Evaluate-for')
+    method_referTo = neomodel.RelationshipTo('Method', 'Refer-to')
+    method_isA = neomodel.RelationshipTo('Method', 'Is-a')
+    method_appearIn = neomodel.RelationshipTo('Method', 'Appear-in')
 
     #relation from Material to ... 
     material_usedFor = neomodel.RelationshipTo('Material', 'Used-for')
@@ -96,88 +96,89 @@ class BaseEntity(neomodel.StructuredNode):
     paper_referTo = neomodel.RelationshipTo('Paper', 'Refer-to')
     paper_isA = neomodel.RelationshipTo('Paper', 'Is-a')
     paper_appearIn = neomodel.RelationshipTo('Paper', 'Appear-in')
-    
+
+
 class Task(BaseEntity):
-    task_usedFor = neomodel.RelationshipTo('Task', 'Used-for',model=UsedFor)
-    task_partOf = neomodel.RelationshipTo('Task', 'Part-of',model=PartOf)
-    task_featureOf = neomodel.RelationshipTo('Task', 'Feature-of',model=FeatureOf)
-    task_compare = neomodel.RelationshipTo('Task', 'Compare',model=Compare)
-    task_hyponymOf = neomodel.RelationshipTo('Task', 'Hyponym-of',model=HyponymOf)
-    task_evaluateFor = neomodel.RelationshipTo('Task', 'Evaluate-for',model=EvaluateFor)
-    # task_referTo = neomodel.RelationshipTo('Task', 'Refer-to',model=ReferTo)
-    task_isA = neomodel.RelationshipTo('Task', 'Is-a',model=IsA)
-    task_appearIn = neomodel.RelationshipTo('Task', 'Appear-in',model=AppearIn)
+    task_usedFor = neomodel.RelationshipTo('Task', 'Used-for')
+    task_partOf = neomodel.RelationshipTo('Task', 'Part-of')
+    task_featureOf = neomodel.RelationshipTo('Task', 'Feature-of')
+    task_compare = neomodel.RelationshipTo('Task', 'Compare')
+    task_hyponymOf = neomodel.RelationshipTo('Task', 'Hyponym-of')
+    task_evaluateFor = neomodel.RelationshipTo('Task', 'Evaluate-for')
+    # task_referTo = neomodel.RelationshipTo('Task', 'Refer-to')
+    task_isA = neomodel.RelationshipTo('Task', 'Is-a')
+    task_appearIn = neomodel.RelationshipTo('Task', 'Appear-in')
 
 class Method(BaseEntity):
-    method_usedFor = neomodel.RelationshipTo('Method', 'Used-for',model=UsedFor)
-    method_partOf = neomodel.RelationshipTo('Method', 'Part-of',model=PartOf)
-    method_featureOf = neomodel.RelationshipTo('Method', 'Feature-of',model=FeatureOf)
-    method_compare = neomodel.RelationshipTo('Method', 'Compare',model=Compare)
-    method_hyponymOf = neomodel.RelationshipTo('Method', 'Hyponym-of',model=HyponymOf)
-    method_evaluateFor = neomodel.RelationshipTo('Method', 'Evaluate-for',model=EvaluateFor)
-    # method_referTo = neomodel.RelationshipTo('Method', 'Refer-to',model=ReferTo)
-    method_isA = neomodel.RelationshipTo('Method', 'Is-a',model=IsA)
-    method_appearIn = neomodel.RelationshipTo('Method', 'Appear-in',model=AppearIn)
+    method_usedFor = neomodel.RelationshipTo('Method', 'Used-for')
+    method_partOf = neomodel.RelationshipTo('Method', 'Part-of')
+    method_featureOf = neomodel.RelationshipTo('Method', 'Feature-of')
+    method_compare = neomodel.RelationshipTo('Method', 'Compare')
+    method_hyponymOf = neomodel.RelationshipTo('Method', 'Hyponym-of')
+    method_evaluateFor = neomodel.RelationshipTo('Method', 'Evaluate-for')
+    # method_referTo = neomodel.RelationshipTo('Method', 'Refer-to')
+    method_isA = neomodel.RelationshipTo('Method', 'Is-a')
+    method_appearIn = neomodel.RelationshipTo('Method', 'Appear-in')
 
 
 class Material(BaseEntity):
-    material_usedFor = neomodel.RelationshipTo('Material', 'Used-for',model=UsedFor)
-    material_partOf = neomodel.RelationshipTo('Material', 'Part-of',model=PartOf)
-    material_featureOf = neomodel.RelationshipTo('Material', 'Feature-of',model=FeatureOf)
-    material_compare = neomodel.RelationshipTo('Material', 'Compare',model=Compare)
-    material_hyponymOf = neomodel.RelationshipTo('Material', 'Hyponym-of',model=HyponymOf)
-    material_evaluateFor = neomodel.RelationshipTo('Material', 'Evaluate-for',model=EvaluateFor)
-    # material_referTo = neomodel.RelationshipTo('Material', 'Refer-to',model=ReferTo)
-    material_isA = neomodel.RelationshipTo('Material', 'Is-a',model=IsA)
-    material_appearIn = neomodel.RelationshipTo('Material', 'Appear-in',model=AppearIn)
+    material_usedFor = neomodel.RelationshipTo('Material', 'Used-for')
+    material_partOf = neomodel.RelationshipTo('Material', 'Part-of')
+    material_featureOf = neomodel.RelationshipTo('Material', 'Feature-of')
+    material_compare = neomodel.RelationshipTo('Material', 'Compare')
+    material_hyponymOf = neomodel.RelationshipTo('Material', 'Hyponym-of')
+    material_evaluateFor = neomodel.RelationshipTo('Material', 'Evaluate-for')
+    # material_referTo = neomodel.RelationshipTo('Material', 'Refer-to')
+    material_isA = neomodel.RelationshipTo('Material', 'Is-a')
+    material_appearIn = neomodel.RelationshipTo('Material', 'Appear-in')
 
 class OtherScientificTerm(BaseEntity):
-    otherScientificTerm_usedFor = neomodel.RelationshipTo('OtherScientificTerm', 'Used-for',model=UsedFor)
-    otherScientificTerm_partOf = neomodel.RelationshipTo('OtherScientificTerm', 'Part-of',model=PartOf)
-    otherScientificTerm_featureOf = neomodel.RelationshipTo('OtherScientificTerm', 'Feature-of',model=FeatureOf)
-    otherScientificTerm_compare = neomodel.RelationshipTo('OtherScientificTerm', 'Compare',model=Compare)
-    otherScientificTerm_hyponymOf = neomodel.RelationshipTo('OtherScientificTerm', 'Hyponym-of',model=HyponymOf)
-    otherScientificTerm_evaluateFor = neomodel.RelationshipTo('OtherScientificTerm', 'Evaluate-for',model=EvaluateFor)
-    # otherScientificTerm_referTo = neomodel.RelationshipTo('OtherScientificTerm', 'Refer-to',model=ReferTo)
-    otherScientificTerm_isA = neomodel.RelationshipTo('OtherScientificTerm', 'Is-a',model=IsA)
-    otherScientificTerm_appearIn = neomodel.RelationshipTo('OtherScientificTerm', 'Appear-in',model=AppearIn)
+    otherScientificTerm_usedFor = neomodel.RelationshipTo('OtherScientificTerm', 'Used-for')
+    otherScientificTerm_partOf = neomodel.RelationshipTo('OtherScientificTerm', 'Part-of')
+    otherScientificTerm_featureOf = neomodel.RelationshipTo('OtherScientificTerm', 'Feature-of')
+    otherScientificTerm_compare = neomodel.RelationshipTo('OtherScientificTerm', 'Compare')
+    otherScientificTerm_hyponymOf = neomodel.RelationshipTo('OtherScientificTerm', 'Hyponym-of')
+    otherScientificTerm_evaluateFor = neomodel.RelationshipTo('OtherScientificTerm', 'Evaluate-for')
+    # otherScientificTerm_referTo = neomodel.RelationshipTo('OtherScientificTerm', 'Refer-to')
+    otherScientificTerm_isA = neomodel.RelationshipTo('OtherScientificTerm', 'Is-a')
+    otherScientificTerm_appearIn = neomodel.RelationshipTo('OtherScientificTerm', 'Appear-in')
 
 class Metric(BaseEntity):
-    metric_usedFor = neomodel.RelationshipTo('Metric', 'Used-for',model=UsedFor)
-    metric_partOf = neomodel.RelationshipTo('Metric', 'Part-of',model=PartOf)
-    metric_featureOf = neomodel.RelationshipTo('Metric', 'Feature-of',model=FeatureOf)
-    metric_compare = neomodel.RelationshipTo('Metric', 'Compare',model=Compare)
-    metric_hyponymOf = neomodel.RelationshipTo('Metric', 'Hyponym-of',model=HyponymOf)
-    metric_evaluateFor = neomodel.RelationshipTo('Metric', 'Evaluate-for',model=EvaluateFor)
-    # metric_referTo = neomodel.RelationshipTo('Metric', 'Refer-to',model=ReferTo)
-    metric_isA = neomodel.RelationshipTo('Metric', 'Is-a',model=IsA)
-    metric_appearIn = neomodel.RelationshipTo('Metric', 'Appear-in',model=AppearIn)
+    metric_usedFor = neomodel.RelationshipTo('Metric', 'Used-for')
+    metric_partOf = neomodel.RelationshipTo('Metric', 'Part-of')
+    metric_featureOf = neomodel.RelationshipTo('Metric', 'Feature-of')
+    metric_compare = neomodel.RelationshipTo('Metric', 'Compare')
+    metric_hyponymOf = neomodel.RelationshipTo('Metric', 'Hyponym-of')
+    metric_evaluateFor = neomodel.RelationshipTo('Metric', 'Evaluate-for')
+    # metric_referTo = neomodel.RelationshipTo('Metric', 'Refer-to')
+    metric_isA = neomodel.RelationshipTo('Metric', 'Is-a')
+    metric_appearIn = neomodel.RelationshipTo('Metric', 'Appear-in')
 
 class Generic(BaseEntity):
-    generic_usedFor = neomodel.RelationshipTo('Generic', 'Used-for',model=UsedFor)
-    generic_partOf = neomodel.RelationshipTo('Generic', 'Part-of',model=PartOf)
-    generic_featureOf = neomodel.RelationshipTo('Generic', 'Feature-of',model=FeatureOf)
-    generic_compare = neomodel.RelationshipTo('Generic', 'Compare',model=Compare)
-    generic_hyponymOf = neomodel.RelationshipTo('Generic', 'Hyponym-of',model=HyponymOf)
-    generic_evaluateFor = neomodel.RelationshipTo('Generic', 'Evaluate-for',model=EvaluateFor)
-    # generic_referTo = neomodel.RelationshipTo('Generic', 'Refer-to',model=ReferTo)
-    generic_isA = neomodel.RelationshipTo('Generic', 'Is-a',model=IsA)
-    generic_appearIn = neomodel.RelationshipTo('Generic', 'Appear-in',model=AppearIn)
+    generic_usedFor = neomodel.RelationshipTo('Generic', 'Used-for')
+    generic_partOf = neomodel.RelationshipTo('Generic', 'Part-of')
+    generic_featureOf = neomodel.RelationshipTo('Generic', 'Feature-of')
+    generic_compare = neomodel.RelationshipTo('Generic', 'Compare')
+    generic_hyponymOf = neomodel.RelationshipTo('Generic', 'Hyponym-of')
+    generic_evaluateFor = neomodel.RelationshipTo('Generic', 'Evaluate-for')
+    # generic_referTo = neomodel.RelationshipTo('Generic', 'Refer-to')
+    generic_isA = neomodel.RelationshipTo('Generic', 'Is-a')
+    generic_appearIn = neomodel.RelationshipTo('Generic', 'Appear-in')
 
 class Abbreviation(BaseEntity):
-    abbreviation_referTo = neomodel.RelationshipTo('Abbreviation', 'Refer-to',model=ReferTo)
-    abbreviation_isA = neomodel.RelationshipTo('Abbreviation', 'Is-a',model=IsA)
+    abbreviation_referTo = neomodel.RelationshipTo('Abbreviation', 'Refer-to')
+    abbreviation_isA = neomodel.RelationshipTo('Abbreviation', 'Is-a')
 
 
 class Paper(BaseEntity):
-    paper_isA = neomodel.RelationshipTo('Paper', 'Is-a',model=IsA)
+    paper_isA = neomodel.RelationshipTo('Paper', 'Is-a')
     
 
 class BaseRelation(neomodel.StructuredRel):
     # properties
     weight = FloatProperty(default=0)
     create_at = DateTimeProperty(
-        default=datetime.now() # no specific timezone 
+        default=lambda: datetime.now() # no specific timezone 
     )
 
 class HyponymOf(BaseRelation):
