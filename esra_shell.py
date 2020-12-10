2c9339ce6b96c1aa09e7ca4257b9b933445cccc1
tongplw <bangkok.tong@gmail.com>
2020-12-11 00:56:03 +0700

    try bm25 okapi

--- esra_shell.py
+++ esra_shell.py
@@ -57,6 +57,9 @@ class EsraShell(cmd.Cmd):
     
 from src.graph_database import GraphDatabase
 import re
+from rank_bm25 import BM25Okapi
+import numpy as np
+import time
 
 if __name__ == '__main__':
     esra_shell = EsraShell()
@@ -64,8 +67,11 @@ if __name__ == '__main__':
 
     search_text = 'bert'
     print('Search text:', search_text)
+    
     keywords = gs.text_preprocessing(search_text)
+    t = time.time()
     r = gs.search(keywords)
+    print('Time:', time.time() - t, 's')
     for i in r:
         print(i)
     
@@ -83,3 +89,20 @@ if __name__ == '__main__':
     #     print('*'*100)
 
     gdb.find_path(['bert', 'attention'], 'MASS: Masked Sequence to Sequence Pre-training for Language Generation')
+    
+    # # BM25
+    # print('='*100)
+    # nodes = gdb.get_all_entities('Paper')
+    # corpus = [n.abstract.lower() for n in nodes]
+    # tokenized_corpus = [doc.split() for doc in corpus]
+    # bm25 = BM25Okapi(tokenized_corpus)
+    # t = time.time()
+    # tokenized_query = search_text.split()
+    # doc_scores = bm25.get_scores(tokenized_query)
+    # ind = np.argpartition(doc_scores, -10)[-10:]
+    # res_ind = ind[np.argsort(doc_scores[ind])][::-1]
+    # print('Time:', time.time() - t, 's')
+    # for i in res_ind:
+    #     score = doc_scores[i]
+    #     paper = nodes[i]
+    #     print(score, paper.cc, paper.name)
