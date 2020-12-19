a379fd81e6981d04ac7dd18f52e1280dbff4605f
tongplw <bangkok.tong@gmail.com>
2020-12-19 23:33:44 +0700

    add completion

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
+import json
 from src import graph_search as gs
 from flask import Flask, jsonify, request
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 
-@app.route('/test')
-def test():
-    return 'test'
+SWAGGER_URL = '/docs'
+API_URL = '/swagger'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Test application"
+    },
+)
+app.register_blueprint(swaggerui_blueprint)
 
-@app.route('/', methods=['POST'])
-def fetch_query():
-    query = request.json.get('query', None)
+@app.route('/swagger')
+def swagger():
+    with open('swagger.json') as f:
+        swagger_json = json.load(f)
+        return swagger_json
+    
+@app.route('/complete')
+def complete():
+    query = request.args.get("q")
     if not query:
         return jsonify({"msg": "Missing query parameter"}), 400
     
@@ -19,9 +34,9 @@ def fetch_query():
         out = gs.text_autocomplete(temp_text)
         if out:
             n_skip = temp_text.count(' ')
-            out = [' '.join(word.split()[n_skip:]) for word in out]
+            out = [' '.join(word.split()) for word in out]
             break
-    return out
+    return {'sentences': out}
     
     papers = gs.search(query, mode='popularity')
     response = {'papers': papers}
